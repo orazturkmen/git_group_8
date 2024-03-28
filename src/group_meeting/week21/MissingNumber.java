02345676a5c998b73152e745901899b8353458b2
oraz <hudayberdiyevoraz@gmail.com>
2024-03-28 15:39:09 +0100

    week21 MissingNumber solution

---
+++ src/group_meeting/week21/MissingNumber.java
@@ -0,0 +1,56 @@
+package group_meeting.week21;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] ar = {0,1,2,3,5};
+        System.out.println("missing number: " + missingNum(ar));
+
+        int[] num1 = {0,1};
+        System.out.println("missing number: " + missingNum(num1));
+
+        int[] num2 = {9,4,5,8,6,1,3,2,0};
+        System.out.println("missing number: " + missingNum(num2));
+    }
+
+
+    public static int missingNum(int[] array){
+        int n = array.length;
+
+        int[] ints = new int[n+1];
+        for (int j : array) {
+            ints[j]++;
+        }
+
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 0) n = i;
+        }
+        return n;
+    }
+}
+/*
+Question-1 Missing Number
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in
+the range that is missing from the array.
+Example 1:
+Input: nums = [3, 0, 1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0, 3]. 2 is the missing
+number in the range since it does not appear in nums.
+Example 2:
+Input: nums = [0, 1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0, 2]. 2 is the missing
+number in the range since it does not appear in nums.
+Example 3:
+Input: nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0, 9]. 8 is the missing
+number in the range since it does not appear in nums.
+Constraints:
+ n == nums.length
+ 1 <= n <= 104
+ 0 <= nums[i] <= n
+ All the numbers of nums are unique.
+ */
