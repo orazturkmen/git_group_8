98512ac3775313fade6321fe04a0e6641e9546a1
oraz <hudayberdiyevoraz@gmail.com>
2024-05-19 22:09:52 +0200

    week27 task PlusOne solution

---
+++ src/group_meeting/week27/PlusOne.java
@@ -0,0 +1,77 @@
+package group_meeting.week27;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Map;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        //System.out.println((int)Math.pow(10,2));
+        int[] array = {1,2,3};
+        int sum = 0;
+        int l = array.length-1;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i]*(int)Math.pow(10, l--);
+        }
+        System.out.println(sum);
+
+        sum = sum +1;
+        String str = String.valueOf(sum);
+
+        for (int i = 0; i < str.length(); i++) {
+           array[i] = Character.getNumericValue(str.charAt(i));
+        }
+        System.out.println(Arrays.toString(array));
+
+
+        System.out.println(Arrays.toString(plusOne(new int[]{4, 2, 9})));
+        System.out.println(Arrays.toString(plusOne(new int[]{9})));
+
+
+
+        BigInteger num = new BigInteger(str);
+        System.out.println(num);
+        System.out.println(num.add(BigInteger.ONE));
+        //System.out.println(BigInteger.ONE);
+    }
+
+    public static int[] plusOne(int[] array){
+        int sum = 0;
+        int l = array.length;
+        for (int j : array) {
+            sum += j * (int) Math.pow(10, --l);
+        }
+        ++sum;
+
+        String str = String.valueOf(sum);
+        int[] arr = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = Character.getNumericValue(str.charAt(i));
+        }
+
+        return arr;
+    }
+}
+/*
+Question-1 Plus One
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the
+integer. The digits are ordered from most significant to least significant in left-to-right order. The large
+integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+Example 1:
+Input: digits = [1,2,3] Output: [1,2,4]
+Explanation: The array represents the integer 123. Incrementing by one gives 123 + 1 = 124. Thus, the result
+should be [1,2,4].
+Example 2:
+Input: digits = [4,3,2,1] Output: [4,3,2,2]
+Explanation: The array represents the integer 4321. Incrementing by one gives 4321 + 1 = 4322. Thus, the
+result should be [4,3,2,2].
+Example 3:
+Input: digits = [9] Output: [1,0]
+Explanation: The array represents the integer 9. Incrementing by one gives 9 + 1 = 10. Thus, the result should
+be [1,0].
+Constraints:
+ 1 <= digits.length <= 100
+ 0 <= digits[i] <= 9
+ digits does not contain any leading 0's.
+ */
